457a233df133680d8c3fc355f8aa2abb4ba07c79
J-DouglasLira <jdouglira@gmail.com>
2022-02-04 17:18:55 -0300

    box question style

---
+++ quizz-app/script.js
@@ -0,0 +1,42 @@
+const QUIZ_DATA = [
+  {
+    question: 'How old are you?',
+    a: '10',
+    b: '20',
+    c: '30',
+    d: '40',
+    correct: 'c'
+  },
+  {
+    question: 'What is the best programming language?',
+    a: 'Python',
+    b: 'Java',
+    c: 'Javascript',
+    d: 'C',
+    correct: 'b'
+  },
+  {
+    question: 'who is the president of Brazil?',
+    a: 'Lula',
+    b: 'Bolsonaro',
+    c: 'Dilma',
+    d: 'Temer',
+    correct: 'b'
+  },
+  {
+    question: 'What does HTML stand for?',
+    a: 'Hypertext Markup Language',
+    b: 'Cascading Style Sheet',
+    c: 'Jason Object Notation',
+    d: 'Application Programing interface',
+    correct: 'a'
+  },
+  {
+    question: 'What year was JavaScript launched?',
+    a: '1996',
+    b: '1995',
+    c: '1994',
+    d:'none of the above',
+    correct: 'b'
+  }
+]
