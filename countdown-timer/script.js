7df238d3d68b71e255e66e4f46b1386c4e0e4540
J-DouglasLira <jdouglira@gmail.com>
2022-02-04 11:32:24 -0300

    defer attribute was inserted in script tag and countdown timer was linked to html

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -1,17 +1,28 @@
+const daysEl = document.getElementById("day");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
+
 const newYears = '1 Jan 2023';
 
 function countdown(){
+
   const newYearsDate = new Date(newYears);
   const currentDate = new Date();
 
   const totalSeconds = (newYearsDate -  currentDate)/1000;
 
-  const secs = Math.floor(totalSeconds%60);
-  const mins = Math.floor(totalSeconds/60) % 60;
+  const days = Math.floor(totalSeconds/3600/24);
   const hours = Math.floor(totalSeconds/3600) % 24;
-  const days = Math.floor(totalSeconds/86400);
-
+  const minutes = Math.floor(totalSeconds/60) % 60;
+  const seconds = Math.floor(totalSeconds)%60;
+    
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = hours;
+  minsEl.innerHTML = minutes;
+  secondsEl.innerHTML = seconds;
   
+    
 }
 //initial call 
 
