e3d4e92a48d8a666a797aba050f475148f7f12b0
J-DouglasLira <jdouglira@gmail.com>
2022-02-04 10:42:27 -0300

    countdown javascript added

---
+++ countdown-timer/script.js
@@ -0,0 +1,24 @@
+const newYears = '1 Jan 2023';
+
+function countdown(){
+  const newYearsDate = new Date(newYears);
+  const currentDate = new Date();
+
+  const totalSeconds = (newYearsDate -  currentDate)/1000;
+
+  const secs = Math.floor(totalSeconds%60);
+  const mins = Math.floor(totalSeconds/60) % 60;
+  const hours = Math.floor(totalSeconds/3600) % 24;
+  const days = Math.floor(totalSeconds/86400);
+
+  
+  console.log(days,hours,mins,secs);
+
+
+}
+//initial call 
+
+countdown();
+
+setInterval(countdown,1000); // a method to calls a function at specified intervals in miliseconds;
+
