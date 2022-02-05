98d94c2f8b0c0baedc9cc60f5a3d6b965815de1c
J-DouglasLira <jdouglira@gmail.com>
2022-02-05 17:06:30 -0300

    inserted questions

--- quizz-app/script.js
+++ quizz-app/script.js
@@ -40,3 +40,38 @@ const QUIZ_DATA = [
     correct: 'b'
   }
 ]
+
+
+
+
+const questionEl = document.getElementById("question")
+const a_text = document.getElementById("a_text");
+const b_text =document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitButton = document.getElementById('submit');
+
+
+let currentQuiz = 0;
+
+loadQuiz();
+
+
+function loadQuiz() {
+  const currentQuizData = QUIZ_DATA[currentQuiz];
+  questionEl.innerHTML = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+  
+}
+
+submitButton.addEventListener("click", ()=> {
+  currentQuiz++;
+  if(currentQuiz<QUIZ_DATA.length){
+    loadQuiz();
+  } else {
+    alert('You finished');
+  }
+})
